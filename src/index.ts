96b430e07cdfc202754094ad5b8e730aad7aae97
soyocar <royomek@gmail.com>
2022-05-17 16:40:12 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ btnEnv.addEventListener("click", () => {
   nombre = prompt("Ingrese el nombre del alumno: ");
   while (nombre !== "") {
     notaprac = Number(prompt("Ingrese la nota de 'Practicas': "));
-    if (notaprac > 0 && notaprac <= 10) {
+    if (notaprac >= 0 && notaprac <= 10) {
       notaprac = Number(notaprac * 10) / 100;
       notaprob = Number(prompt("Ingrese la nota de 'Problemas': "));
-      if (notaprob > 0 && notaprob <= 10) {
+      if (notaprob >= 0 && notaprob <= 10) {
         notaprob = Number(notaprob * 50) / 100;
         notateo = Number(prompt("Ingrese la nota de 'Teoria': "));
       }
-      if (notateo > 0 && notateo <= 10) {
+      if (notateo >= 0 && notateo <= 10) {
         notateo = Number(notateo * 40) / 100;
       }
       notafinal = Number(notaprac + notaprob + notateo);
