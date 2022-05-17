b8c673671ca19da0b64344bf9c87ffd908490a6c
soyocar <royomek@gmail.com>
2022-05-17 16:00:13 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+let btnEnv = document.getElementById("enviar");
+let notaprac, notaprob, notateo, notafinal: number;
+let nombre: string;
+btnEnv.addEventListener("click", () => {
+  nombre = prompt("Ingrese el nombre del alumno: ");
+  while (nombre != "") {
+    notaprac = Number(prompt("Ingrese la nota de 'Practicas': "));
+    notaprob = Number(prompt("Ingrese la nota de 'Problemas': "));
+    notateo = Number(prompt("Ingrese la nota de 'Teoria': "));
+    notaprac = Number(notaprac * 10) / 100;
+    notaprob = Number(notaprob * 50) / 100;
+    notateo = Number(notateo * 40) / 100;
+    notafinal = Number(notaprac + notaprob + notateo);
+    console.log("La nota final del alumno " + nombre + "es" + notafinal);
+    nombre = prompt("Ingrese el nombre del alumno: ");
+  }
+});
