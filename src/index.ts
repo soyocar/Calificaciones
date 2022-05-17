2599a4e9e00253f57c690a1104ca214485e615a5
soyocar <royomek@gmail.com>
2022-05-17 16:07:44 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ let notaprac, notaprob, notateo, notafinal: number;
 let nombre: string;
 btnEnv.addEventListener("click", () => {
   nombre = prompt("Ingrese el nombre del alumno: ");
-  while (nombre != "") {
+  while (nombre !== "") {
     notaprac = Number(prompt("Ingrese la nota de 'Practicas': "));
     notaprob = Number(prompt("Ingrese la nota de 'Problemas': "));
     notateo = Number(prompt("Ingrese la nota de 'Teoria': "));
@@ -12,7 +12,8 @@ btnEnv.addEventListener("click", () => {
     notaprob = Number(notaprob * 50) / 100;
     notateo = Number(notateo * 40) / 100;
     notafinal = Number(notaprac + notaprob + notateo);
-    console.log("La nota final del alumno " + nombre + "es" + notafinal);
+    //console.log("La nota final del alumno " + nombre + "es" + notafinal);
+    alert(`La nota final del alumno ${nombre} es ${notafinal}`);
     nombre = prompt("Ingrese el nombre del alumno: ");
   }
 });
